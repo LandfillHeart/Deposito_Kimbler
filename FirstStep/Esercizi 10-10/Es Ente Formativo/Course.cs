cbf3371d74abb63e5ea556c1a70ac383d2d4e958
LandfillHeart <kimbleredward@gmail.com>
2025-10-10 17:14:02 +0200

    Esercizio Ente Formativo
    - Architettura di classe astratta Course e concrete InPersonCourse e OnlineCourse
    - Implementazione metodi concreti DeliverCourse e PrintDetails

---
+++ FirstStep/Esercizi 10-10/Es Ente Formativo/Course.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStep.Esercizi_10_10.Es_Ente_Formativo
+{
+	public abstract class Course
+	{
+		private string title;
+		private int lengthHours;
+
+		public string Title => title;
+		public int LengthHours
+		{
+			get => lengthHours;
+			private set
+			{
+				lengthHours = Math.Max(value, 1);
+			}
+		}
+
+		public abstract void DeliverCourse();
+		public abstract void PrintDetails();
+
+		public Course(string title, int legthHours)
+		{
+			this.title = title;
+			this.lengthHours = legthHours;
+		}
+
+		public override string ToString()
+		{
+			return $"Titolo Corso: {title} - Durata (Ore): {lengthHours}";
+		}
+	}
+
+	public class InPersonCourse : Course
+	{
+		private string classroom;
+		private int maxSeats;
+
+		public int MaxSeats
+		{
+			get => maxSeats;
+			private set
+			{
+				maxSeats = Math.Max(value, 1);
+			}
+		}
+
+		public InPersonCourse(string title, int legthHours, string classroom, int maxSeats) : base(title, legthHours)
+		{
+			this.classroom = classroom;
+			MaxSeats = maxSeats;
+		}
+
+		public override void DeliverCourse()
+		{
+			Console.WriteLine($"Diamo inizio al corso in presenza: {Title}");
+			Console.WriteLine($"Il corso oggi si svolge in aula {classroom}");
+		}
+
+		public override void PrintDetails()
+		{
+			Console.WriteLine(ToString());
+		}
+
+		public override string ToString()
+		{
+			return $"{base.ToString()} - Aula: {classroom} - Posti Disponibili: {maxSeats}";
+		}
+	}
+
+	public class OnlineCourse : Course
+	{
+		private string platform;
+		private string link;
+
+		public OnlineCourse(string title, int legthHours, string platform, string link) : base(title, legthHours)
+		{
+			this.platform = platform;
+			this.link = link;
+		}
+
+		public override void DeliverCourse()
+		{
+			Console.WriteLine($"Diamo inizio al corso online: {Title}");
+			Console.WriteLine($"Per accedere entrare a questo link {platform}: {link}");
+		}
+
+		public override void PrintDetails()
+		{
+			Console.WriteLine(ToString());
+		}
+
+		public override string ToString()
+		{
+			return $"{base.ToString()} - Piattaforma: {platform} - Link: {link}";
+		}
+	}
+
+}
---
+++ FirstStep/Esercizi 10-10/Es Ente Formativo/Es_EnteFormativo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStep.Esercizi_10_10.Es_Ente_Formativo
+{
+	internal class Es_EnteFormativo : IGenericExercise
+	{
+		string IGenericExercise.Name => "Esercizio Ente Formativo";
+
+		private List<Course> availableCourses = new();
+
+		public void Execute()
+		{
+
+		}
+	}
+}
