754352402aaa5bb0b59b3efbb67235359801cc3d
LandfillHeart <kimbleredward@gmail.com>
2025-10-20 16:07:35 +0200

    Dependency Injection

--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+	public static void Main(string[] args)
+	{
+		GreetingService myService = new GreetingService(new ConsoleGreeter());
+	}
+}
+
+public class GreetingService
+{
+	private IGreeter myGreeter;
+	public GreetingService(IGreeter greeter)
+	{
+		myGreeter = greeter;
+		greeter.Greet();
+	}
+}
+
+public interface IGreeter
+{
+	public void Greet();
+}
+
+public class ConsoleGreeter : IGreeter
+{
+	public void Greet()
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
