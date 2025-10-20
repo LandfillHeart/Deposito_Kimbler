458d5aac392e9051ab003c8a7cd3ac4160974375
LandfillHeart <kimbleredward@gmail.com>
2025-10-20 16:19:05 +0200

    DI - Payment Gateway

--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -2,10 +2,20 @@
 {
 	public static void Main(string[] args)
 	{
-		GreetingService myService = new GreetingService(new ConsoleGreeter());
+		Console.WriteLine("Scegli un processore del pagamento: \n 0 - PayPall \n 1 - Stripe");
+		int choice = int.Parse(Console.ReadLine());
+		if (choice == 0)
+		{
+			new PaymentProcessor(new PayPallGateway());
+		}
+		else 
+		{
+			new PaymentProcessor(new StripeGateway());
+		}
 	}
 }
 
+#region GreetingService
 public class GreetingService
 {
 	private IGreeter myGreeter;
@@ -28,3 +38,37 @@ public class ConsoleGreeter : IGreeter
 		Console.WriteLine("Hello, World!");
 	}
 }
+#endregion
+
+#region PaymentProcessor
+public class PaymentProcessor
+{
+	private IPaymentGateway gateway;
+	public PaymentProcessor(IPaymentGateway gateway)
+	{
+		this.gateway = gateway;
+		gateway.DisplayGateway();
+	}
+}
+
+public interface IPaymentGateway
+{
+	public void DisplayGateway();
+}
+
+public class PayPallGateway : IPaymentGateway
+{
+	public void DisplayGateway()
+	{
+		Console.WriteLine("Hai scelto PayPall");
+	}
+}
+public class StripeGateway : IPaymentGateway
+{
+	public void DisplayGateway()
+	{
+		Console.WriteLine("Hai scelto Stripe");
+	}
+}
+
+#endregion
