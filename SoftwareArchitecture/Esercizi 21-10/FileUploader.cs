c4281284ab227c559731fa39b1c1fe6dc2a5cdec
LandfillHeart <kimbleredward@gmail.com>
2025-10-21 11:02:44 +0200

    Setter Injection - FileUploader
    - Una classe FileUploader che usa setter injection per ricevere il servizio di salvataggio file
    - Controlli sul flusso dei dati: metodi specifici per l'accesso al componente IStorageService per evitare errori di null reference

---
+++ SoftwareArchitecture/Esercizi 21-10/FileUploader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareArchitecture.Esercizi_21_10
+{
+	public class FileUploader
+	{
+		// private get: non vogliamo dare accesso dall'esterno a StorageService, visto che potrebbero provare a leggere da un componente null
+		// piuttosto, creiamo metodi di accesso alle proprietà di StorageService (come RetrieveFile)
+		public IStorageService StorageService { private get; set; }
+		public FileUploader() {  }
+
+		public void UploadFile(string file)
+		{
+			if(StorageService == null)
+			{
+				Console.WriteLine("Non hai accesso al salvataggio dei file!");
+				return;
+			}
+
+			StorageService.Store(file);
+		}
+
+		public string RetrieveFile()
+		{
+			if(StorageService == null)
+			{
+				return "Error: Nessun accesso a lettura dei file";
+			}
+			return StorageService.File;
+		}
+	}
+
+	public class FileUploaderApp : IExecutable
+	{
+		public void Run()
+		{
+			FileUploader myUploader = new FileUploader();
+			myUploader.UploadFile("C:/unaImmagine.png");
+			Console.WriteLine(myUploader.RetrieveFile());
+
+			myUploader.StorageService = new DiskStorage();
+			myUploader.UploadFile("C:/unoScript.cs");
+			Console.WriteLine(myUploader.RetrieveFile());
+
+			myUploader.StorageService = new RAM_Storage();
+			myUploader.UploadFile("C:/unaList.json");
+			Console.WriteLine(myUploader.RetrieveFile());
+			
+			RAM_Storage storage = new RAM_Storage();
+			Console.WriteLine($"File in RAM: {storage.File}");
+		}
+	}
+
+	public interface IStorageService
+	{
+		public string File { get; }
+		public void Store(string image);
+	}
+
+	public class DiskStorage : IStorageService
+	{
+		public string File { get; private set; }
+		public void Store(string file)
+		{
+			File = file;
+		}
+	}
+
+	public class RAM_Storage : IStorageService
+	{
+		private static string storedFile;
+		public string File => storedFile;
+		public void Store(string file)
+		{
+			storedFile = file;
+		}
+	}
+}
--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -9,31 +9,15 @@ namespace SoftwareArchitecture
 		public static void Main(string[] args)
 		{
 			// in entrambi i casi stiamo facendo riferimento allo stesso singleton, ma nel secondo abbiamo la possibilità di sovrascrivere config se necessario
-
-			Printer myPrinter = new Printer();
-			myPrinter.Print();
-
-			myPrinter.Logger = new SimpleLogger("App Stampante: Hai stampato");
-			myPrinter.Print();
-
-			myPrinter.Logger = new SimpleLogger("App Desktop: Hai stampato");
-			myPrinter.Print();
-
-			#region CSTR Injection con Decorator
-			/*
-			OrderService firstOrderService = new OrderService(new LoggerService(), new List<string>() { 
-				"Ordine #12 in arrivo domani!", "Ordine #98 Annullato", "Ordine #77 in arrivo tra 3 giorni" });
-
-			IAppConfig defaultConfig = OrderAppContext.Instance;
-			IAppConfig betaConfig = new BetaContext(defaultConfig);
-
-			OrderService secondOrderService = new OrderService(new LoggerService(betaConfig), new List<string>() { 
-				"Ordine #8 annullato!", "Ordine #11 in arrivo tra 6 giorni"});
-
-			firstOrderService.GetOrderStatus();
-			secondOrderService.GetOrderStatus();
-			*/
-			#endregion
+			
+			FileUploaderApp myApp = new FileUploaderApp();
+			myApp.Run();
 		}
+	}
+
+	// piuttosto che riscrivere il main ogni volta, preferisco creare un oggetto di questa interfaccia e farlo girare nel main
+	public interface IExecutable
+	{
+		public void Run();
 	}
 }
