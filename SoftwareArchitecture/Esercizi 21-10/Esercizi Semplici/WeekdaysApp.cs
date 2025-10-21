575ecc612d4009f1bebf1135492f37cdc7276493
LandfillHeart <kimbleredward@gmail.com>
2025-10-21 17:09:59 +0200

    Aggiunto commenti enum flags

--- SoftwareArchitecture/Esercizi 21-10/Esercizi Semplici/WeekdaysApp.cs
+++ SoftwareArchitecture/Esercizi 21-10/Esercizi Semplici/WeekdaysApp.cs
@@ -10,7 +10,7 @@ namespace SoftwareArchitecture.Esercizi_21_10.Esercizi_Semplici
 	{
 		public void Run()
 		{
-			Weekdays testDays = Weekdays.Thursday | Weekdays.Friday;
+			Weekdays testDays = Weekdays.Thursday | Weekdays.Friday; // 01000 + 10000 = 11000
 
 			Console.WriteLine($"Nel corso C# facciamo i test i giorni: {testDays.ToString()}");
 		}
@@ -19,10 +19,10 @@ namespace SoftwareArchitecture.Esercizi_21_10.Esercizi_Semplici
 	[Flags]
 	public enum Weekdays
 	{
-		Monday = 1, 
-		Tuesday = 2, 
-		Wednesday = 4,
-		Thursday = 8,
-		Friday = 16,
+		Monday = 1,		// 00001
+		Tuesday = 2,	// 00010
+		Wednesday = 4,	// 00100
+		Thursday = 8,	// 01000
+		Friday = 16,	// 10000
 	}
 }
