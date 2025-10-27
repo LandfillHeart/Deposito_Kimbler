8ecdfd404872b00749be1fb5f82c1de0da4274ca
LandfillHeart <kimbleredward@gmail.com>
2025-10-27 16:53:51 +0100

    Correzione esercizio 4

--- Query_SQL/test pratico 27-10/esercizio_4.sql
+++ Query_SQL/test pratico 27-10/esercizio_4.sql
@@ -6,5 +6,5 @@ USE MyBookstore;
 SELECT Books.title, Sales.date_of_sale, Books.price, Sales.quantity
 FROM Books
 RIGHT JOIN Sales ON Sales.fk_book_id = Books.id
-WHERE Sales.date_of_sale BETWEEN '2024-01-01' AND '2024-06-31'
+WHERE Sales.date_of_sale BETWEEN '2024-01-01' AND '2024-06-30'
 AND Sales.store LIKE '%Inc%'; 
