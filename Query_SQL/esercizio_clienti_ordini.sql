70f833412fa89cada238f60ec159b3ecb5eb36f2
LandfillHeart <kimbleredward@gmail.com>
2025-10-27 12:26:28 +0100

    Clienti e Ordini parte 2 - Es. 1: Clienti Attivi

--- Query_SQL/esercizio_clienti_ordini.sql
+++ Query_SQL/esercizio_clienti_ordini.sql
@@ -9,6 +9,23 @@ CREATE TABLE IF NOT EXISTS Clients (
 INSERT INTO MyShippingCompany.Clients (username, city) VALUES
 	('Ray', 'Napoli'),
     ('Sergio', 'Bari'),
+    ('Matteo', 'Lucca'),
+    ('Mirko', 'Milano'),
+    ('Amedeo', 'Caserta'),
+    ('Francesco', 'Napoli'),
+    ('Salvatore', 'Napoli'),
+    ('Armando', 'Bari'),
+    ('Arvin', 'Napoli'),
+    ('Lucrezia', 'Venezia'),
+    ('Rosaria', 'Salerno'),
+    ('Federica', 'Roma'),
+    ('Lucia', 'Roma'),
+    ('Camilla', 'Milano'),
+    ('Sara', 'Firenze'),
+    ('Daniela', 'Francia'),
+    ('Andrea', 'Spagna'),
+    ('Tecla', 'Germania'),
+    ('Aspen', 'Spagna'),
     ('Luca', 'Bologna');
 
 
@@ -24,14 +41,29 @@ INSERT INTO MyShippingCompany.Orders (fk_client_id, order_date, cost) VALUES
     (1, "2005-07-19", 142.50),
     (1, "2009-10-30", 188.42),
     (2, "2024-05-02", 201.20),
+    (19, "2001-09-23", 999.99),
+    (8, "2007-11-23", 89.99),
+    (11, "2015-03-12", 23089.12),
+    (14, "2025-10-27", 11.99),
+    (3, "2002-01-08", 10990.22),
+    (7, "2019-11-23", 100),
+    (11, "2003-11-21", 1407.75),
+    (13, "2016-10-20", 13.99),
+    (6, "2004-11-23", 12.05),
+    (14, "1999-08-12", 15.65),
+    (17, "2006-06-06", 10.99),
+    (7, "2001-09-23", 999.99),
+    (6, "2022-11-19", 67.67),
+    (19, "2014-10-01", 13450.90),
     (1, "2025-01-12", 199.80);
 
 -- controlla che sia tutto inserito correttamente --
 SELECT * FROM MyShippingCompany.Clients;
 SELECT * FROM MyShippingCompany.Orders;
 
--- pulisci la tabella dai doppioni --
+-- elimina la tabella per inserire di nuovo tutti i dati senza doppioni --
 DROP TABLE MyShippingCompany.Orders;
+DROP TABLE MyShippingCompany.Clients;
 
 -- INNER JOIN mostra tutti clienti che hanno effettuato un ordine --
 SELECT Clients.username, Orders.order_date, Orders.cost
---
+++ Query_SQL/esercizio_clienti_ordini_2.sql
@@ -0,0 +1,7 @@
+USE MyShippingCompany;
+
+-- Clienti Attivi, ordini effettuati, totale costo ordini --
+SELECT Clients.username, COUNT(Orders.id) AS Number_Of_Orders, SUM(Orders.cost) AS Total_Spent
+FROM Orders
+INNER JOIN Clients ON Clients.id = Orders.fk_client_id
+GROUP BY Clients.id;
